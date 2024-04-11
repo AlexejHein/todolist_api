f32e40bb7cd619164e05289ccc4cc37efd4ebbe8
Alexej Hein <alexej.hein@aol.de>
2024-04-11 07:27:23 +0200

    add restapi

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from todo.models import Todo
 
 
+class Userserializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username',]
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'created_at', 'user']
